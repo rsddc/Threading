878dd6a67beb63814d73e5e1d36745a25e3d9da4
rsddc <rakesh.ranu@gmail.com>
2022-09-21 10:41:18 +0530

    First commit-Java Puzzle for output test

---
+++ src/com/LoopTest.java
@@ -0,0 +1,14 @@
+
+
+public class LoopTest {
+
+    public static void main(String[] args) {
+        int i =0;
+        while (i < 3){
+            i++;
+            System.out.println("value of i "+i);
+
+        }while (i >2);
+        System.out.println("Here..............");
+    }
+}
